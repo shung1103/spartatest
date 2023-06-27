c0fd1bb9d09194af004d05d4377b8ae928e3b92d
shung1103 <shung1103@hanmail.net>
2023-06-27 12:14:08 +0900

    validation으로 회원가입 시 아이디, 비밀번호, 이메일 검증

--- src/main/java/com/sparta/blog/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/blog/dto/SignupRequestDto.java
@@ -1,13 +1,22 @@
 package com.sparta.blog.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignupRequestDto {
+    @NotBlank
+    @Size(min = 4, max = 10, message = "아이디는 최소 4자 이상, 10자 이하로 해주세요.")
     private String username;
+    @NotBlank
+    @Size(min = 8, max = 15, message = "비밀번호는 최소 8자 이상, 15자 이하로 해주세요.")
     private String password;
+    @Email(message = "email@address.com 의 형식으로 입력해 주세요.")
+    @NotBlank
     private String email;
     private boolean admin = false;
     private String adminToken = "";
--- src/main/java/com/sparta/blog/service/UserService.java
+++ src/main/java/com/sparta/blog/service/UserService.java
@@ -29,16 +29,10 @@ public class UserService {
         String password = passwordEncoder.encode(requestDto.getPassword());
 
         // 아이디 입력 제한 확인
-        if (username.length() < 4 || username.length() > 10) {
-            throw new IllegalArgumentException("아이디는 최소 4자 이상, 10자 이하로 해주세요.");
-        }
         if (!Pattern.matches("^[a-z0-9]*$",username)) {
             throw new IllegalArgumentException("아이디는 알파벳 소문자(a~z), 숫자(0~9)로 해주세요.");
         }
         // 비밀번호 입력 제한 확인
-        if (requestDto.getPassword().length() < 8 || username.length() > 15) {
-            throw new IllegalArgumentException("비밀번호는 최소 8자 이상, 15자 이하로 해주세요.");
-        }
         if (!Pattern.matches("^[a-zA-Z0-9]*$",requestDto.getPassword())) {
             throw new IllegalArgumentException("비밀번호는 알파벳 대소문자(a~z, A~Z), 숫자(0~9)로 해주세요.");
         }
